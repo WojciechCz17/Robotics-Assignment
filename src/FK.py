222d9c518d1801aeb2b56202fa5008c84daa1f14
Wojiy <acczin67@gmail.com>
2019-11-21 17:51:07 +0000

    FK working edit

--- src/FK.py
+++ src/FK.py
@@ -15,7 +15,7 @@ class FK:
     def __init__(self):
         self.thing = 1
 
-    def transformation(theta, alpha, a, d):
+    def transformation(self, theta, alpha, a, d):
         # DH_params = [alpha, a, d, theta]
 
         trans_d = np.array([[1, 0, 0, 0],
@@ -44,11 +44,10 @@ class FK:
         
         return T
 
-    def quick_trans(theta, alpha, a, d):
+    def quick_trans(self, theta, alpha, a, d):
         T = [[math.cos(theta), -math.sin(theta)*math.cos(alpha), math.sin(theta)*math.sin(alpha), a*math.cos(theta)],
             [math.sin(theta), math.cos(theta)*math.cos(alpha), -math.cos(theta)*math.sin(alpha), a*math.sin(theta)],
-            [0, math.sin(alpha), math.cos(theta), d],
-            [0, 0, 0, 1]]
+            [0, math.sin(alpha), math.cos(theta), d]]
 
         return T
     # def detect_end_effector(self,image):
@@ -59,9 +58,9 @@ class FK:
 
 def main():
     fk = FK()
-    T = fk.transformation(1, 0, 3, 0)
+    T = fk.quick_trans(1, 0, 3, 0)
     print(T)
-    x = np.dot(T, [2, 8, 3])
+    x = np.dot([2, 8, 3], T)
     print(x)
     print("HI")
 
